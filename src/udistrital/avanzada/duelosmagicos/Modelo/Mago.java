25a379238217f8e8f1aabadff743609cdf84dce7
ymbermudezt <230941078+ymbermudezt@users.noreply.github.com>
2025-10-27 14:48:50 -0500

    fix(modelo) eliminado metodos y campos de logica de negocio de modelo Mago.java

--- src/udistrital/avanzada/duelosmagicos/Modelo/Mago.java
+++ src/udistrital/avanzada/duelosmagicos/Modelo/Mago.java
@@ -1,12 +1,11 @@
 package udistrital.avanzada.duelosmagicos.Modelo;
 
 import java.util.Random;
-import udistrital.avanzada.duelosmagicos.Control.CampoDuelo;
 
 /**
  * Clase Mago.
  * <p>
- * Descripción:
+ * Clase que representa a un mago que participa en el torneo
  * </p>
  *
  * @author Diego
@@ -17,74 +16,17 @@ public class Mago implements Runnable {
 
     private String nombre;
     private String casa;
-    private int puntos;
-    private boolean aturdido;
-    private Mago rival;
-    //cantidad de hechizos lanzados
-    private int hechizosLanzados;
-    private CampoDuelo campo;
-
-    /**
-     * Constructor vacio
-     */
-    public Mago() {
-    }
 
     public Mago(String nombre, String casa) {
         this.nombre = nombre;
         this.casa = casa;
-        this.puntos = 0;
-        this.aturdido = false;
-        this.rival = null;
-        this.hechizosLanzados = 0;
-        this.campo = null;
     }
 
-    /**
-     *
-     * @param puntos
-     */
-    public void lanzarHechizo(int puntos) {
-        this.puntos += puntos;
-        rival.setAturdido(true);
-    }
-    
-    public boolean estaAturdido() {
-        return aturdido;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setAturdido(boolean aturdido) {
-        this.aturdido = aturdido;
-    }
-
-    public void setCampo(CampoDuelo campo) {
-        this.campo = campo;
-    }
-
-    public Mago getRival() {
-        return rival;
-    }
-
-    public void setRival(Mago rival) {
-        this.rival = rival;
-    }
-
-    @Override
-    public void run() {
-        while (puntos < 250) {
-            if (aturdido) {
-                try {
-                    Thread.sleep(new Random().nextInt(250));
-                } catch (InterruptedException ie) {
-                }
-                aturdido = false;
-            } else {
-                campo.lanzarHechizo(this);
-                try {
-                    Thread.sleep(new Random().nextInt(500));
-                } catch (InterruptedException ie) {
-                }
-            }
-        }
+    public String getCasa() {
+        return casa;
     }
 }
